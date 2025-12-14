8514b4a859e5afba616923f03dbbf54a172b0e93
Ayash-Bera <ayashbera@gmail.com>
2025-12-14 20:34:11 +0530

    tokenisations done with errors

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -28,6 +28,28 @@ class Scanner {
     return tokens;
   }
 
+  private static final Map<String,TokenType> keywords;
+    
+  static{
+    keywords = new Hashmap<>();
+    keywords.put("and",   AND);
+    keywords.put("class",   CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",   FALSE);
+    keywords.put("for",   FOR);
+    keywords.put("fun",   FUN);
+    keywords.put("if",   IF);
+    keywords.put("nil",   NIL);
+    keywords.put("or",   OR);
+    keywords.put("print",   PRINT);
+    keywords.put("return",   RETURN);
+    keywords.put("super",   SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",   VAR);
+    keywords.put("while",   WHILE);
+  }
+
   private void scanTokens() {
     char c = advance();
     switch (c) {
@@ -42,16 +64,16 @@ class Scanner {
       case ';': addToken(SEMICOLON);break;
       case '*': addToken(STAR);break;
       case '!': 
-        addToken(match('=')) ? BANG_EQUAL :BANG);
+        addToken(match('=') ? BANG_EQUAL :BANG);
         break;
       case'=':
-        addToken(match('=')) ? EQUAL_EQUAL : EQUAL);
+        addToken(match('=') ? EQUAL_EQUAL : EQUAL);
         break;
       case'>': 
-        addToken(match('=')) ? LESS_EQUAL :LESS );
+        addToken(match('=') ? LESS_EQUAL :LESS );
         break;
       case '<':
-        addToken(match('=')) ? GREATER_EQUAL : GREATER);
+        addToken(match('=') ? GREATER_EQUAL : GREATER);
         break;
       case '/':
         if (match('/')) {
@@ -73,21 +95,27 @@ class Scanner {
         break;
       case '"': string(); 
         break;
+      case 'o':
+        if (peek()=='r') {
+          addToken(OR);
+        }
+        break;
 
       default:
       if (isDigit(c)) {
         number();
+      }else if (isAlpha(c)) {
+        identifier();
       }else{
         Lox.error(line,"wrong shit get better.");
       }
-        break;
     } 
   }
 
   private boolean match(char expected) {
     if (isAtEnd()) return false;
     if (source.charAt(current) != expected ) return false;
-    current++ //big brain shit right here 
+    current++; //big brain shit right here 
     return true;
   }
 
@@ -134,7 +162,16 @@ class Scanner {
   }
 
   private boolean isDigit(char c){
-    return c >= '0' && c <= '9'
+    return c >= '0' && c <= '9';
+  }
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c<= 'z') ||
+          (c >= 'A' && c<= 'Z') || c== '-'; 
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c); 
   }
 
   private void number() {
@@ -145,4 +182,17 @@ class Scanner {
     }
     addToken(NUMBER,Double.parseDouble(source.substring(start,current)));
   }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  } 
+
+  private void identifier(){
+    while (isAlphaNumeric(peek())) advance();
+    String text = source.substring(start,current);
+    TokenType type = keywords.get(text);
+    if(type == null) type = IDENTIFIER;
+    addToken(type);
+  };
 }
